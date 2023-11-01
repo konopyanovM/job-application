2ab3b1061f923dc32b9f3449c8b5c2ae4b1d947a
Manap <konopyanov.manap@gmail.com>
2023-11-01 17:05:44 +0600

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from './views/HomePage/HomePage';
+import { AuthPage } from './views/AuthPage/AuthPage';
 
 function App() {
   return (
     <div className="App">
+      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/auth" element={<AuthPage />}></Route>
+      </Routes>
     </div>
   );
 }
